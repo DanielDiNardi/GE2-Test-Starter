d89a7f12cf29c3b0d5fbf7364351b3a4c4084c03
Daniel Di Nardi <c18487682@mytudublin.ie>
2022-04-26 09:24:42 +0100

    Started segments, have to make a new branch

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -11,6 +11,16 @@ public class Nematode : MonoBehaviour
     void Awake()
     {
         // Put your code here!
+
+        length = Random.Range(0, 20);
+
+        for (int i = 0; i < length; i++)
+        {
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            GameObject instance = Instantiate(sphere);
+            instance.transform.parent = transform;
+            instance.transform.position = transform.position - (transform.forward * i);
+        }
     }
 
 
