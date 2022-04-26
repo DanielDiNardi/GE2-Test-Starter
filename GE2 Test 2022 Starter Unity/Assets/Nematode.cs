383b01431fadab962e13487cd250938818c0a735
Daniel Di Nardi <c18487682@mytudublin.ie>
2022-04-26 10:31:54 +0100

    worm tapers

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Nematode : MonoBehaviour
 {
-    public int length = 5;
+    public int length = 6;
 
     public Material material;
 
@@ -12,14 +12,24 @@ public class Nematode : MonoBehaviour
     {
         // Put your code here!
 
-        length = Random.Range(5, 20);
+        // length = Random.Range(5, 20);
 
         for (int i = 0; i < length; i++)
         {
             GameObject segment = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             segment.transform.parent = transform;
             segment.transform.position = transform.position - (transform.forward * i);
-            segment.transform.scale = new Vector3()
+            if(i <= (length / 2))
+            {
+                float frontHalf = (1f / ((float)length / 2f)) * ((float)i + 1f);
+
+                segment.transform.localScale = new Vector3(frontHalf, frontHalf, 1f);
+            }
+            else
+            {
+                float backHalf = (1f / ((float)length / 2f)) * (((float)length + 1f) - ((float)i + 1f));
+                segment.transform.localScale = new Vector3(backHalf, backHalf, 1f);
+            }
         }
     }
 
